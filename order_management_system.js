5a93cb1a2a3c612adf3b10058806551886989da0
tcoolican <tcoolican@usf.edu>
2024-09-22 19:26:17 -0400

    Initialize inventory with product objects

--- order_management_system.js
+++ order_management_system.js
@@ -1,1 +1,26 @@
+//Coding Challenge #5
+//Task 1: Create an Inventory Array of Product Objects
+// Inventory array with product objects
+const inventory = [
+    {
+        name: 'Riddell Axiom',
+        price: 759.99,
+        quantity: 50},
+    {
+        name: 'Schutt F7',
+        price: 499.99,
+        quantity: 100},
+    {
+        name: 'Riddell Speeflex',
+        price: 499.99,
+        quantity: 100},
+    {
+        name: 'VICIS Zero2',
+        price: 799.99,
+        quantity: 50}
+//These are the most commonly used football helmets
+];
+console.log(inventory);
+
+//Task 2: Create an Orders Array of Order Objects
 
